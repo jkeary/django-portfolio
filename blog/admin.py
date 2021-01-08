c50ac86138a56b430e33e1c1461bcaf3cc800834
jkeary <jkeary@gmail.com>
2021-01-08 16:21:40 -0500

    boblablas lawblog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from django.contrib import admin
+from .models import Blog
+
+# Register your models here.
+admin.site.register(Blog)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+# create blog model
+# title 
+# pub date
+# body
+# image
+class Blog(models.Model):
+    title = models.CharField(max_length=250)
+    pub_date = models.DateTimeField(auto_now=False, auto_now_add=True)
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+# add blog app to settings
+
+# create migration
+
+# migrate
+
+# add to admin
