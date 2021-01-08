cf427e52268a7c7037e32678af562ee4a8e65cd4
jkeary <jkeary@gmail.com>
2021-01-07 19:14:40 -0500

    job model

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,6 @@
+# https://docs.djangoproject.com/en/2.0/ref/models/fields/
 from django.db import models
 
-# Create your models here.
+class Job(models.Model):
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
