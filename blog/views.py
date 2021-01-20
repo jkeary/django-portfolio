3fdb1d1d35dc8320748238b1e50082f5ffc19e7e
jkeary <jkeary@gmail.com>
2021-01-20 17:55:48 -0500

    a blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
+def allblogs(request):
+    posts = Blog.objects
+    return render(request, 'blog/allblogs.html', {'posts': posts})
